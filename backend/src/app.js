84e4f014f9838ed049cf22b524637d7979838152
Arpan Dhandar <arpandhandar.77@gmail.com>
2026-03-29 02:37:18 +0530

    refactor: remove unused constants and simplify MongoDB connection logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,17 @@
 import express from "express"
 import cors from "cors"
+import noteModel from "./models/note.model.js";
 
 const app = express();
+
+app.use(express.json({ limit: "16kb" }));
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
+
+
 
 export default app 
