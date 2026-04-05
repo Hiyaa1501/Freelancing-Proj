c896aed183a94e3939fd1a18b340beddb4846b62
Arpan Dhandar <arpandhandar.77@gmail.com>
2026-04-05 16:34:55 +0530

    feat: add JWT middleware for token verification

---
+++ backend/src/middleware/jwt.middleware.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+import config from "../config/config.js"
+import { ApiError } from "../utils/ApiError.util"
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.accessToken;
+    if (!token) return next(ApiError(401, "You are not authenticated!"))
+
+        jwt.verify(token, config.JWT_SERCRET, async (err, payload) => {
+            if (err) return next(ApiError(403, "Token is not valid!"))
+                req.userId = payload.id,
+                req.isSeller = payload.isSeller
+                next()
+        })
+    }
+
