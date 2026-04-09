907ef454c07489416baeafc7fdb4b8f6a3f8bb2c
Arpan Dhandar <arpandhandar.77@gmail.com>
2026-04-09 22:54:49 +0530

    fix: correct error handling and response status in createGig and deleteGig methods

--- backend/src/controllers/gig.controller.js
+++ backend/src/controllers/gig.controller.js
@@ -6,7 +6,7 @@ import { asyncHandler } from "../utils/asyncHandler.util.js"
 export const createGig = asyncHandler(async(req, res, next) => {
    
     if (!req.isSeller){
-        return new ApiError(403, "Only sellers can create a gig!")
+        throw new ApiError(403, "Only sellers can create a gig!")
     }
 
     const newGig = new Gig({
@@ -15,22 +15,40 @@ export const createGig = asyncHandler(async(req, res, next) => {
     })
 
     const savedGig = await newGig.save();
-    resizeBy.status().json(new ApiResponse(201, savedGig,"Gig saved successfully!"))
+    res.status().json(new ApiResponse(201, savedGig,"Gig saved successfully!"))
 })
 
 export const deleteGig = asyncHandler(async(req, res, next) => {
     const gig = await Gig.findById(req.params.id)
     if (gig.userId !== req.userId) {
-        return new ApiError(403, "You can delete only your gig!")}
+        throw new ApiError(403, "You can delete only your gig!")}
 
     await Gig.findByIdAndDelete(req.params.id)
-    resizeBy.status(200).json(new ApiResponse(200, {}, "Gig has been deleted!"))
+    res.status(200).json(new ApiResponse(200, {}, "Gig has been deleted!"))
     })
 
 export const getGig = asyncHandler(async(req, res, next) => {
-    
+    const gig = await Gig.findById(req.params.id)
+    if (!gig) {
+        throw new ApiError(404, "Gig not found!")    
+    }
+    res.status(200).json(new ApiResponse(200, gig, ""))
 })
 
 export const getGigs = asyncHandler(async(req, res, next) => {
-    
+    const q = req.query;
+    const filters = {
+        ...(q.userId && { userId: q.userId }),
+        ...(q.cat && { cat: q.cat}),
+        ...((q.min || q.max) && {
+            price: {
+                ...(q.min && { $qt: q.min }),
+                ...(q.max && { $lt: q.max })
+            },
+        })
+        ...(q.search && { title: { $regex: q.search, $options: "i"}}),
+    }
+
+    const gigs = await Gig.find(filters).sort({ [q.sort]: -1 })
+    res.status(200).json(new ApiResponse(200, gigs, ""))
 })
