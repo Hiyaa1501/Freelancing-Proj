c5f3f37c1e7fc2e55870c6e77037ca995292dd7a
Arpan Dhandar <arpandhandar.77@gmail.com>
2026-04-09 19:56:39 +0530

    feat: implement gig routes and controller for creating, deleting, and retrieving gigs

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import authRoute from "./routes/auth.route.js"
 import userRoute from "./routes/user.route.js"
-// import gigRoute from "./routes/gig.route.js"
+import gigRoute from "./routes/gig.route.js"
 // import orderRoute from "./routes/order.route.js"
 // import conversationRoute from "./routes/conversation.route.js"
 // import messageRoute from "./routes/message.route.js"
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // Routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-// app.use("/api/gigs", gigRoute);
+app.use("/api/gigs", gigRoute);
 // app.use("/api/orders", orderRoute);
 // app.use("/api/conversations", conversationRoute);
 // app.use("/api/messages", messageRoute);
---
+++ backend/src/controllers/gig.controller.js
@@ -0,0 +1,31 @@
+import Gig from "../models/gig.model.js"
+import { ApiError } from "../utils/ApiError.util.js"
+import { ApiResponse } from "../utils/ApiResponse.util.js"
+import { asyncHandler } from "../utils/asyncHandler.util.js"
+
+export const createGig = asyncHandler(async(req, resizeBy, next) => {
+   
+    if (!req.isSeller){
+        return new ApiError(403, "Only sellers can create a gig!")
+    }
+
+    const newGig = new Gig({
+        userId: req.userId,
+        ...req.body,
+    })
+
+    const savedGig = await newGig.save();
+    resizeBy.status().json(new ApiResponse(201, savedGig,"Gig saved successfully!"))
+})
+
+export const deleteGig = asyncHandler(async(req, resizeBy, next) => {
+    
+})
+
+export const getGig = asyncHandler(async(req, resizeBy, next) => {
+    
+})
+
+export const getGigs = asyncHandler(async(req, resizeBy, next) => {
+    
+})
